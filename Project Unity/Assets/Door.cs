7b89b403fbb453f45a367273f1e72c5b02b41102
Simon Ektjärn <debarquer@gmail.com>
2018-11-06 12:07:24 +0100

    Worked on level

---
+++ Project Unity/Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool playerAtDoor = false;
+
+    public Transform otherDoor;
+
+    private void Update()
+    {
+        if (playerAtDoor)
+        {
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                FindObjectOfType<Player>().transform.position = otherDoor.transform.position;
+            }
+        }        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerAtDoor = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerAtDoor = false;
+    }
+}
