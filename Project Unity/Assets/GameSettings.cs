1a027d165e0764a3e32f6f97a79a0edcec952d32
Simon Ektjärn <debarquer@gmail.com>
2018-11-09 09:32:50 +0100

    Made timer prefab

---
+++ Project Unity/Assets/GameSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+
+    public bool alternateVoicePack;
+
+    public UnityEngine.UI.Text audioSettingsText;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+
+        string settingsString = "Current voice pack: " + (alternateVoicePack ? "Eva" : "Cabal")
+        audioSettingsText.text = settingsString;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
